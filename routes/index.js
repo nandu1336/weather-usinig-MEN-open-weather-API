2635ab5fd2d5332fb1d58c954f8ec5c851b248a9
unknown <nk732100@gmail.com>
2019-12-06 21:11:35 +0530

    Next deployment ready with log in log out fixed

--- routes/index.js
+++ routes/index.js
@@ -4,60 +4,62 @@ var path = require('path');
 var mysql = require('mysql')
 
 var login = false;
+var username = null;
+
 var conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'nandu',
-  password: 'password',
-  database: 'firstapp'
-})
-
-
+    host: "maria-database.mariadb.database.azure.com",
+    user: "appadmin@maria-database",
+    password: "Nandu@141",
+    database: "weather",
+    port: 3306,
+    //ssl: { ca: fs.readFileSync({ ca - cert filename }) }
+});
+conn.connect();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Weather App', user: username });
 });
 
-router.get('/login',function(req,res,next){
-	res.render('login',{title : 'Login'});
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Login', user: username });
 });
 
-router.post('/login',function(req,res,next){
-  
-  var username = req.body.username;
-  var password = req.body.password;
-  conn.connect();
-  if(conn){
-    conn.query('SELECT * FROM users WHERE username = ? and password = ?',[username,password],function(err,rows,fields){
-      if(err)
-      throw err;
-      if(rows.length > 0){
-        login = true;
-        res.render('weather',{title:'Weather',user:username});
-      }
-      else{
-        res.redirect('/login');
-      }
+router.post('/login', function(req, res, next) {
+
+    username = req.body.username;
+    var password = req.body.password;
+
+
+    conn.query('SELECT * FROM users WHERE username = ? and password = ?', [username, password], function(err, rows, fields) {
+        if (err)
+            throw err;
+        if (rows.length > 0) {
+            login = true;
+            res.render('weather', { title: 'Weather', user: username });
+        } else {
+            res.redirect('/login');
+        }
     });
-  }
 })
-router.get('/weather',function(req,res){
-  if(login == false){
-    res.redirect('/login');
-  }
-  res.render('weather',{title:'Weather'});
-})
-
-router.get('/logout',function(req,res){
-  if(login == true){
-    login = false;
-    res.redirect('/login');
-  }
+router.get('/weather', function(req, res) {
+    if (login == false) {
+        res.redirect('/login');
+    }
+    res.render('weather', { title: 'Weather', user: username });
 })
 
-router.get('/menu',function(req,res){
-  if(login == false){
-    res.redirect('/login');
-  }
-  res.render('menu',{title:'menu'});
+router.get('/logout', function(req, res) {
+    if (login == true) {
+        login = false;
+        username = null;
+        res.redirect('/login');
+    }
+})
+
+router.get('/menu', function(req, res) {
+    if (login == false) {
+        res.redirect('/login');
+    }
+    res.render('menu', { title: 'menu', user: username });
 })
 module.exports = router;
